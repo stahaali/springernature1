715dfc7cbd2c74be67e735cb9468be78c880fd6b
Taha <staha218@hotmail.com>
2024-12-02 18:40:29 +0500

    commint ok

--- assets/js/main.js
+++ assets/js/main.js
@@ -50,3 +50,59 @@ $(document).ready(function()
     });
 
 });
+
+
+$('.whitepapers-slider').owlCarousel({
+    loop:true,
+    margin:20,
+    dots:true,
+    autoplay: true,
+    autoPlaySpeed: 300,
+    responsive:{
+        0:{
+            items:1,
+        },
+        768:{
+            items:2,
+        },
+        1000:{
+            items:3,
+        }
+    }
+});
+
+$('.readypublish-slider').owlCarousel({
+    loop:true,
+    margin:20,
+    dots:true,
+    autoplay: true,
+    autoPlaySpeed: 400,
+    responsive:{
+        0:{
+            items:1,
+        },
+        768:{
+            items:2,
+        },
+        1000:{
+            items:4,
+        }
+    }
+});
+
+$('.readypublish-logoslider').owlCarousel({
+    loop:true,
+    margin:20,
+    dots:true,
+    responsive:{
+        0:{
+            items:1,
+        },
+        768:{
+            items:2,
+        },
+        1000:{
+            items:4,
+        }
+    }
+});
