7a0d698ffbbdbc38d82a6be59f3cb4d8ab4f98b0
asteriksdevteam <devteam@asteriksdigital.com>
2024-12-01 23:19:50 -0800

    commint ok

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,8 @@ $('.toggle').click(function()
     $('.menu-list').toggleClass('show');
 });
 
-$(document).ready(function() {
+$(document).ready(function() 
+{
     $(".content").hide().slice(0, 3).show();
     $('.minusicon').hide();
     $('.load-button a').click(function(e) {
@@ -24,4 +25,18 @@ $(document).ready(function() {
             $(this).html('<i class="fa-solid fa-plus plusicon"></i> Show More');
         }
     });
+});
+
+$('.toggle2').click(function() 
+{
+    $('.collapse-menu2').toggleClass('show');
+
+    if ($('.collapse-menu2').hasClass('show')) 
+    {
+        $('.mobile-menu').addClass('fixed-menu');
+    } 
+    else 
+    {
+        $('.mobile-menu').removeClass('fixed-menu');
+    }
 });
